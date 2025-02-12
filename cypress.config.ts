0e686ad725fd82b81642910b6e5661e6c694defc
Oleksandra Hresniuk <ct.oleksandrahresniuk@waterford.org>
2025-02-12 13:38:12 +0200

    fix: e2e fails during deploy

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://promo-pro-21485.web.app/',
     setupNodeEvents(on, config) {},
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 20000,
+    responseTimeout: 20000,
   },
 });
